f985f6368c2fbb1f2c2413e91a567d2f5292ac1f
Sowon Kim <sso626262@naver.com>
2024-11-06 23:04:22 +0900

    Update search.jsx

--- chapter6/movie-mission/src/pages/search.jsx
+++ chapter6/movie-mission/src/pages/search.jsx
@@ -1,12 +1,60 @@
 import styled from "styled-components";
+import useCustomFetch from "../hooks/useCustomFetch";
+import View from "../components/View";
+import { useEffect, useState } from "react";
 
 const SearchPage = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [query, setQuery] = useState("");
+  const [isMovieExists, setIsMovieExists] = useState(false);
+
+  const { data, isLoading, isError } = useCustomFetch(
+    `/search/movie?query=${query}&include_adult=false&language=ko-KR&page=1`
+  );
+
+  const handleSearch = () => {
+    setQuery(inputValue);
+  };
+
+  useEffect(() => {
+    if (data.data?.results.length > 0) {
+      setIsMovieExists(true);
+    } else {
+      setIsMovieExists(false);
+    }
+  }, [data]);
+
+  if (isLoading) {
+    return (
+      <Container>
+        <h1 style={{ color: "white" }}>로딩 중 입니다...</h1>
+      </Container>
+    );
+  }
+  if (isError) {
+    return (
+      <Container>
+        <h1 style={{ color: "white" }}>에러 발생...</h1>
+      </Container>
+    );
+  }
+
   return (
     <Container>
       <InputWrapper>
-        <StyledInput placeholder="영화 제목을 입력해주세요..." />
-        <Btn>검색</Btn>
+        <StyledInput
+          type="text"
+          value={inputValue}
+          placeholder="영화 제목을 입력해주세요..."
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Btn onClick={handleSearch}>검색</Btn>
       </InputWrapper>
+      {query && !isMovieExists ? (
+        <p style={{ color: "white" }}>검색어 '{query}'에 대한 검색 결과가 없습니다.</p>
+      ) : (
+        <View movies={data} />
+      )}
     </Container>
   );
 };
@@ -28,6 +76,7 @@ const InputWrapper = styled.div`
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
+  margin: 20px 0;
 `;
 const StyledInput = styled.input`
   padding: 10px;
